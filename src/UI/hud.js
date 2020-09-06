136006744987b668ed9b38b37a49b0949cf7318c
alexandros <alexandros.katsarakis@gmail.com>
2020-09-07 00:45:39 +0300

    Added the first blocks that interact with objects

--- src/UI/hud.js
+++ src/UI/hud.js
@@ -1,55 +1,90 @@
 import bb from '../utils/blackboard.js'
+Blockly.Blocks['colour_change'] = {
+    init: function() {
+      this.appendValueInput('VALUE')
+          .setCheck('Colour')
+          .appendField('Change Colour');
+      this.setColour(24);
+      this.setTooltip('Change the colour of the object it refers.');
+      this.setHelpUrl('none');
+      this.setPreviousStatement(true);
+      this.setNextStatement(true);
+      return 0;
+    }
+  };
 
-let hudcss = document.createElement('link');
-hudcss.setAttribute('rel','stylesheet');
-hudcss.setAttribute('href','./src/UI/hud.css');
-document.body.appendChild(hudcss);
+  Blockly.JavaScript['colour_change'] = function(block) {
+    var argument0 = Blockly.JavaScript.valueToCode(block, 'VALUE',
+        Blockly.JavaScript.ORDER_FUNCTION_CALL) || '\'\'';
+    return 'bb.fastGet("actions","changeColor")(undefined,'+argument0 + ')';
+  };
+
+function readTextFile(file,onFinish){
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                document.body.insertAdjacentHTML('beforeend',allText);
+                onFinish();
+            }
+        }
+    }
+    rawFile.send(null);
+}
+readTextFile('./src/UI/hud.ahtml',onHudLoaded);
+
+function hudState(){
+    let isVisible = true;
+    function toggleVisibility(){
+        isVisible = !isVisible;
+        let hudChildren = document.querySelectorAll('.hudChild');
+        hudChildren.forEach(element => {
+            element.style.display = (isVisible)?"block":"none";
+            element.style.opacity = (isVisible)?"1":"0";
+        });
+    }
+    return toggleVisibility;
+}
 
 
-let inp = document.createElement("input");
-inp.id = "inputss";
-document.body.appendChild(inp);
+function onHudLoaded(){
+    let toggle = document.getElementById('hudToggle');
+    toggle.addEventListener('click',hudState());
 
-let blocklyDiv = document.createElement('div');
-blocklyDiv.id = 'blocklyDiv';
-blocklyDiv.style.position = "absolute";
-blocklyDiv.style.height = "500px";
-blocklyDiv.style.width = "500px";
-blocklyDiv.style.top = "50px";
-document.body.appendChild(blocklyDiv);
+    
 
-let workspace = Blockly.inject('blocklyDiv',
-{toolbox: document.getElementById('toolbox'),scrollbars: true});
+    let blocklyDiv = document.getElementById('blocklyDiv');
+    blocklyDiv.style.height = "500px";
+    blocklyDiv.style.width = "500px";
 
-Blockly.JavaScript.addReservedWords('code');
+    Blockly.inject('blocklyDiv',{toolbox: document.getElementById('toolbox'),scrollbars: true});
+    
+    Blockly.JavaScript.addReservedWords('code');
 
-let focused = document.createElement("div");
-focused.id = "focusedObjText";
-document.body.appendChild(focused);
+    objMenuButton.addEventListener('click',()=>{
+        if(document.getElementById('objMenu'))document.getElementById('objMenu').remove();
+        let objMenu = document.createElement("div");
+        objMenu.id = "objMenu";
+        objMenu.classList += "hudChild";
+        document.body.appendChild(objMenu);
 
-let objMenuButton = document.createElement("div");
-objMenuButton.id = "objMenuButton";
-document.body.appendChild(objMenuButton);
-
-
-
-objMenuButton.addEventListener('click',()=>{
-    if(document.getElementById('objMenu'))document.getElementById('objMenu').remove();
-    let objMenu = document.createElement("div");
-    objMenu.id = "objMenu";
-    document.body.appendChild(objMenu);
-
-    let focusedObj = bb.fastGet('state','focusedObject');
-    if(!focusedObj)return;
-    let options = bb.fastGet('liveObjects',focusedObj).getOptions();
-    options.map(opt=>{
-        let i = document.createElement('div');
-        i.className = "objMenuButton";
-        i.innerHTML = opt;
-        i.addEventListener('click',()=>{
-            bb.fastGet('actions',opt)(focusedObj);
+        let focusedObj = bb.fastGet('state','focusedObject');
+        if(!focusedObj)return;
+        let options = bb.fastGet('liveObjects',focusedObj).getOptions();
+        options.map(opt=>{
+            let i = document.createElement('div');
+            i.className = "objMenuButton";
+            i.innerHTML = opt;
+            i.addEventListener('click',()=>{
+                bb.fastGet('actions',opt)(focusedObj);
+            })
+            objMenu.appendChild(i);
         })
-        objMenu.appendChild(i);
-    })
-    console.log(options);
-});
+        console.log(options);
+    });
+}
--- src/actions/changeColor.js
+++ src/actions/changeColor.js
@@ -1,8 +1,13 @@
 import bb from '../utils/blackboard.js'
 
-function changeColor(objName){
-    let obj = bb.fastGet('liveObjects',objName);
-    if(obj)obj.setColor("#"+document.getElementById("inputss").value);
+function changeColor(objName,colour){
+    if(!colour){
+        let obj = bb.fastGet('liveObjects',objName);
+        if(obj)obj.setColor("#ffffff");
+    }else{
+        let obj = bb.fastGet('liveObjects',bb.fastGet('state','focusedObject'));
+        if(obj)obj.setColor(colour);
+    }
 }
 
 bb.fastSet('actions','changeColor',changeColor)
