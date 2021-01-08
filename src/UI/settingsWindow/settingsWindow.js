70022177de7d25eb6a651fef65b12f517235434d
Alexandros Katsarakis <csd3577@csd.uoc.gr>
2021-01-08 13:53:48 +0200

    added categories in settings window with Vue

--- src/UI/settingsWindow/settingsWindow.js
+++ src/UI/settingsWindow/settingsWindow.js
@@ -9,52 +9,56 @@ function closeSettingsWindow(){
 }
 
 const settings = {
-    mainColor: {
-        onChange: (ev)=>{
-            document.documentElement.style.setProperty('--main-color', ev.target.value);
+    Colors: {
+        mainColor: {
+            onChange: (ev)=>{
+                document.documentElement.style.setProperty('--main-color', ev.target.value);
+            },
+            name: 'mainColor',
+            inputType:'color'
         },
-        name: 'mainColor',
-        inputType:'color'
-    },
-    mainColorHover: {
-        onChange: (ev)=>{
-            document.documentElement.style.setProperty('--main-color-hover', ev.target.value);
+        mainColorHover: {
+            onChange: (ev)=>{
+                document.documentElement.style.setProperty('--main-color-hover', ev.target.value);
+            },
+            name: 'mainColorHover',
+            inputType:'color'
         },
-        name: 'mainColorHover',
-        inputType:'color'
-    },
-    mainColorText: {
-        onChange: (ev)=>{
-            document.documentElement.style.setProperty('--main-color-text', ev.target.value);
+        mainColorText: {
+            onChange: (ev)=>{
+                document.documentElement.style.setProperty('--main-color-text', ev.target.value);
+            },
+            name: 'mainColorText',
+            inputType:'color'
         },
-        name: 'mainColorText',
-        inputType:'color'
-    },
-    secondaryColor: {
-        onChange: (ev)=>{
-            document.documentElement.style.setProperty('--secondary-color', ev.target.value);
+        secondaryColor: {
+            onChange: (ev)=>{
+                document.documentElement.style.setProperty('--secondary-color', ev.target.value);
+            },
+            name: 'secondaryColor',
+            inputType:'color'
         },
-        name: 'secondaryColor',
-        inputType:'color'
-    },
-    secondaryColorHover: {
-        onChange: (ev)=>{
-            document.documentElement.style.setProperty('--secondary-color-hover', ev.target.value);
+        secondaryColorHover: {
+            onChange: (ev)=>{
+                document.documentElement.style.setProperty('--secondary-color-hover', ev.target.value);
+            },
+            name: 'secondaryColorHover',
+            inputType:'color'
         },
-        name: 'secondaryColorHover',
-        inputType:'color'
+        secondaryColorText: {
+            onChange: (ev)=>{
+                document.documentElement.style.setProperty('--secondary-color-text', ev.target.value);
+            },
+            name: 'secondaryColorText',
+            inputType:'color'
+        }
     },
-    secondaryColorText: {
-        onChange: (ev)=>{
-            document.documentElement.style.setProperty('--secondary-color-text', ev.target.value);
-        },
-        name: 'secondaryColorText',
-        inputType:'color'
-    },
-    myName: {
-        onChange: ()=>console.log('b'),
-        name: 'myName',
-        change:'text'
+    Names:{
+        myName: {
+            onChange: ()=>console.log('b'),
+            name: 'myName',
+            change:'text'
+        }
     }
 }
 
@@ -62,34 +66,30 @@ function onSettingsWindowLoaded(){
     document.getElementById('settings-window-background').addEventListener('click',closeSettingsWindow);
 
 
-    // Vue.component('settings-item', {
-    //     props: ['object'],
-    //     template: '<div class="objectItem">{{ object.name }} {{ object.id }}</div>'
-    //   })
-    // let objects = bb.fastGet('Engine','ObjectManager').objects;
-
-    // let vue = new Vue({
-    //     el: '#settings-window',
-    //     data: {
-    //         objects
-    //     },
-    //     methods: {
-    //         changeListItem: () => {
-    //             console.log(objects);
-    //         }
-    //     }
-    // })
+    Vue.component('settings-category', {
+        props: ['settings','catname'],
+        template:`<div class='settings_category'>
+                    <div class='settings_category_name'>{{ catname }}</div>
+                    <settings-item
+                        v-for="set in settings"
+                        v-bind:setting="set"
+                        v-bind:key="set.name"
+                    />
+                </div>`
+    });
 
     Vue.component('settings-item-input', {
         props: ['setting'],
         template: 
         `<input :type="setting.inputType" v-on:input="setting.onChange"></input>`
-      })
+      });
+
     Vue.component('settings-item', {
         props: ['setting'],
         template: 
         `<div class="objectItem">{{ setting.name }} <settings-item-input v-bind:setting="setting" v-bind:key="setting.name"/></div>`
-      })
+      });
+
     let vue = new Vue({
         el: '#settings-window',
         data: {
