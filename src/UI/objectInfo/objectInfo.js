6b8ee4fffdcce23d9633dfb893fcc5c723011868
alexandros <alexandros.katsarakis@gmail.com>
2021-02-12 22:01:11 +0200

    added buttons for object on objectInfo

--- src/UI/objectInfo/objectInfo.js
+++ src/UI/objectInfo/objectInfo.js
@@ -271,6 +271,34 @@ function createFlagsView(flags){
     }
 }
 
+function fillObjectActions(obj){
+    let wrapper = document.getElementById('mainInfoBox-options');
+    wrapper.innerHTML = '';
+    wrapper.insertAdjacentHTML('beforeend', `<svg id="mainInfoBox-options-close" class="mainInfoBox-options-item" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512.001 512.001" style="enable-background:new 0 0 512.001 512.001;" xml:space="preserve"><g><g><path d="M284.286,256.002L506.143,34.144c7.811-7.811,7.811-20.475,0-28.285c-7.811-7.81-20.475-7.811-28.285,0L256,227.717L34.143,5.859c-7.811-7.811-20.475-7.811-28.285,0c-7.81,7.811-7.811,20.475,0,28.285l221.857,221.857L5.858,477.859c-7.811,7.811-7.811,20.475,0,28.285c3.905,3.905,9.024,5.857,14.143,5.857c5.119,0,10.237-1.952,14.143-5.857L256,284.287l221.857,221.857c3.905,3.905,9.024,5.857,14.143,5.857s10.237-1.952,14.143-5.857c7.811-7.811,7.811-20.475,0-28.285L284.286,256.002z"/></g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg>`);
+    
+    let closeBut = document.getElementById('mainInfoBox-options-close');
+    closeBut.onclick = ()=>{
+        bb.fastSet('state', 'focusedObject', undefined);
+    }
+    
+    if(Engine.hasManager('ClipboardManager')){
+        wrapper.insertAdjacentHTML('beforeend', `<svg id="mainInfoBox-options-copy" class="mainInfoBox-options-item" height="512pt" viewBox="-40 0 512 512" width="512pt" xmlns="http://www.w3.org/2000/svg"><path d="m271 512h-191c-44.113281 0-80-35.886719-80-80v-271c0-44.113281 35.886719-80 80-80h191c44.113281 0 80 35.886719 80 80v271c0 44.113281-35.886719 80-80 80zm-191-391c-22.054688 0-40 17.945312-40 40v271c0 22.054688 17.945312 40 40 40h191c22.054688 0 40-17.945312 40-40v-271c0-22.054688-17.945312-40-40-40zm351 261v-302c0-44.113281-35.886719-80-80-80h-222c-11.046875 0-20 8.953125-20 20s8.953125 20 20 20h222c22.054688 0 40 17.945312 40 40v302c0 11.046875 8.953125 20 20 20s20-8.953125 20-20zm0 0"/></svg>`);
+        let copyBut = document.getElementById('mainInfoBox-options-copy');
+        copyBut.onclick = ()=>{
+            Engine.ClipboardManager.copy(obj);
+        }
+    }
+
+
+    wrapper.insertAdjacentHTML('beforeend', `<svg id="mainInfoBox-options-remove" class="mainInfoBox-options-item" height="448pt" viewBox="-69 0 448 448.00446" width="448pt" xmlns="http://www.w3.org/2000/svg"><path d="m283.429688 45.714844h-73.140626v-18.285156c0-15.125-12.304687-27.429688-27.429687-27.429688h-54.855469c-15.125 0-27.429687 12.304688-27.429687 27.429688v18.285156h-73.144531c-15.125 0-27.42578175 12.304687-27.42578175 27.429687v45.710938h18.28515575v301.71875c0 15.125 12.300782 27.429687 27.425782 27.429687h219.429687c15.125 0 27.429688-12.304687 27.429688-27.429687v-301.71875h18.285156v-45.710938c0-15.125-12.304687-27.429687-27.429687-27.429687zm-164.570313-18.285156c0-5.042969 4.097656-9.144532 9.144531-9.144532h54.855469c5.046875 0 9.144531 4.101563 9.144531 9.144532v18.285156h-73.144531zm155.429687 393.144531c0 5.046875-4.097656 9.144531-9.144531 9.144531h-219.429687c-5.042969 0-9.140625-4.097656-9.140625-9.144531v-301.71875h237.714843zm18.285157-320.003907h-274.285157v-27.425781c0-5.042969 4.097657-9.144531 9.140626-9.144531h256c5.046874 0 9.144531 4.101562 9.144531 9.144531zm0 0"/><path d="m210.289062 384.003906c5.054688 0 9.140626-4.089844 9.140626-9.140625v-201.148437c0-5.050782-4.085938-9.144532-9.140626-9.144532-5.054687 0-9.144531 4.09375-9.144531 9.144532v201.148437c0 5.050781 4.089844 9.140625 9.144531 9.140625zm0 0"/><path d="m155.429688 384.003906c5.054687 0 9.144531-4.089844 9.144531-9.140625v-201.148437c0-5.050782-4.089844-9.144532-9.144531-9.144532-5.050782 0-9.140626 4.09375-9.140626 9.144532v201.148437c0 5.050781 4.089844 9.140625 9.140626 9.140625zm0 0"/><path d="m100.574219 384.003906c5.054687 0 9.140625-4.089844 9.140625-9.140625v-201.148437c0-5.050782-4.085938-9.144532-9.140625-9.144532-5.054688 0-9.144531 4.09375-9.144531 9.144532v201.148437c0 5.050781 4.089843 9.140625 9.144531 9.140625zm0 0"/></svg>`);
+    let removeBut = document.getElementById('mainInfoBox-options-remove');
+    removeBut.style.fill = 'red';
+    removeBut.onclick = ()=>{
+        obj.remove();
+        bb.fastSet('state', 'focusedObject', undefined);
+    }
+}
+
 function updateInfo(obj){
     if(!document.getElementById('mainInfoBox'))return;
     if(obj === undefined || bb.fastGet('state','mode') !== 'editing'){
@@ -281,6 +309,8 @@ function updateInfo(obj){
     document.getElementById('mainInfoBox').style.display = "block";
     document.getElementById('mainInfoBox-head-name').innerHTML = 'Name: '+obj.name;
     document.getElementById('mainInfoBox-head-categ').innerHTML = 'Category: '+obj.getCategory();
+
+    fillObjectActions(obj);
 
     let addStateBut = document.getElementById('mainInfoBox-body-states-add');
     addStateBut.onclick = addStateButtonHandler;
