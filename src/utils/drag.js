404a706468cb0878085c1b69475c115ee3fbcc66
Alexandros Katsarakis <alexandros.katsarakis@gmail.com>
2021-07-08 00:17:43 +0300

    Fixed drag with touch

--- src/utils/drag.js
+++ src/utils/drag.js
@@ -2,22 +2,36 @@ import env from '../Engine/renderer/EnvironmentObject.js'
 
 export default function dragElement(elmnt,ev) {
   let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  dragMouseDown();
+  dragMouseDown(ev);
 
   const ratio = env._aspectRatio;
 
   function dragMouseDown(e) {
     e = e || window.event;
-    e.preventDefault();
+    if(e instanceof TouchEvent){    
+      e.clientX = e.touches[0].clientX;
+      e.clientY = e.touches[0].clientY;
+    }else{
+      e.preventDefault();
+    }
     pos3 = e.clientX;
     pos4 = e.clientY;
+
+    document.ontouchend = closeDragElement;
+    document.ontouchmove = elementDrag;
+
     document.onmouseup = closeDragElement;
     document.onmousemove = elementDrag;
   }
 
   function elementDrag(e) {
     e = e || window.event;
-    e.preventDefault();
+    if(e instanceof TouchEvent){    
+      e.clientX = e.touches[0].clientX;
+      e.clientY = e.touches[0].clientY;
+    }else{
+      e.preventDefault();
+    }
 
     pos1 = pos3 - e.clientX;
     pos2 = pos4 - e.clientY;
@@ -35,6 +49,9 @@ export default function dragElement(elmnt,ev) {
   }
 
   function closeDragElement() {
+    document.ontouchend = null;
+    document.ontouchmove = null;
+
     document.onmouseup = null;
     document.onmousemove = null;
   }
